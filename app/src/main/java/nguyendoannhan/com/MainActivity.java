72b286a8c72eaa870854d8df782e581fd4e3e6eb
NguyenDOanNhan <nguyendoannhan.ptit@gmail.com>
2019-03-05 09:23:10 +0700

    tudien

--- app/src/main/java/nguyendoannhan/com/MainActivity.java
+++ app/src/main/java/nguyendoannhan/com/MainActivity.java
@@ -1,13 +1,42 @@
 package nguyendoannhan.com;
 
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv_name;
+    private static final String TAG="Nhan";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_name = (TextView) findViewById(R.id.tv_name);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference mRef = database.getReference("tbl_anh");
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue().toString();
+                tv_name.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG,"failed to readvalue", databaseError.toException());
+            }
+        });
+
     }
+
 }
